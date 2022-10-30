35c70d2ba0af22e41b4092ac7e6bdbef4f3a2b63
Gustavo Corone <gcoronelc@gmail.com>
2022-10-30 09:45:41 -0500

    Clase 2

--- Clase2/GROUP_BY.sql
+++ Clase2/GROUP_BY.sql
@@ -26,4 +26,42 @@ WHERE department_id IS NOT NULL
 GROUP BY department_id
 HAVING count(1) > 10;
 
+select extract(month from sysdate) from dual;
+
+SELECT 
+    department_id,
+    sum(case when extract(month from hire_date)=1 then 1 else 0 end) Enero,
+    sum(case when extract(month from hire_date)=2 then 1 else 0 end) Febrero,
+    sum(case when extract(month from hire_date)=3 then 1 else 0 end) Marzo,
+    sum(case when extract(month from hire_date)=4 then 1 else 0 end) Abril,
+    sum(case when extract(month from hire_date)=5 then 1 else 0 end) Mayo,
+    sum(case when extract(month from hire_date)=6 then 1 else 0 end) Junio,
+    sum(case when extract(month from hire_date)=7 then 1 else 0 end) Julio,
+    sum(case when extract(month from hire_date)=8 then 1 else 0 end) Agosto,
+    sum(case when extract(month from hire_date)=9 then 1 else 0 end) Setiembre,
+    sum(case when extract(month from hire_date)=10 then 1 else 0 end) Octubre,
+    sum(case when extract(month from hire_date)=11 then 1 else 0 end) Noviembre,
+    sum(case when extract(month from hire_date)=12 then 1 else 0 end) diciembre,
+    count(1) total
+FROM hr.employees
+WHERE department_id IS NOT NULL
+GROUP BY department_id;
+
+select * 
+from hr.employees;
+
+
+-- Empleado que tiene el menor salario, mostrar los empates.
+
+select department_id, min(salary) salario_minimo
+from hr.employees
+where department_id is not null
+group by department_id;
+
+
+
+
+
+
+
 
