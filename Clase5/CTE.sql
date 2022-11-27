c58107b91147ea4d891001d3d5f43943c4bc156c
Gustavo Corone <gcoronelc@gmail.com>
2022-11-27 09:27:24 -0500

    Semana 5

--- Clase5/CTE.sql
+++ Clase5/CTE.sql
@@ -13,14 +13,39 @@ select * from CTE;
 WITH 
 CTE1 AS ( 
 	SELECT employee_id, first_name, department_id 
-	FROM employees 
+	FROM hr.employees 
 ), 
 CTE2 AS ( 
 	SELECT department_id, department_name 
-	FROM departments 
+	FROM hr.departments 
 ) 
 SELECT 
 	CTE1.employee_id, CTE1.first_name, CTE2.department_name 
 FROM CTE1 JOIN CTE2 
 ON CTE1.department_id = CTE2.department_id;
+
+
+-- EJERCICIO
+
+WITH
+CTE1 AS (
+	SELECT 
+		IDDEPARTAMENTO, 
+		COUNT(1) EMPLEADOS,
+		SUM(SUELDO) IMPORTE,
+		SUM(NVL(COMISION,0)) COMISION
+	FROM RECURSOS.empleado
+	GROUP BY IDDEPARTAMENTO
+)
+SELECT 
+	D.NOMBRE DEPARTAMENTO,
+	CTE1.EMPLEADOS,
+	CTE1.IMPORTE,
+	CTE1.COMISION,
+	(CTE1.IMPORTE + CTE1.COMISION) TOTAL
+FROM RECURSOS.departamento D 
+JOIN CTE1 ON D.IDDEPARTAMENTO = CTE1.IDDEPARTAMENTO;
+
+
+
 
