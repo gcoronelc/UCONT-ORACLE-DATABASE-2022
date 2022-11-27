ac569547f277d16941e33653660e5eb8089989f1
Gustavo Corone <gcoronelc@gmail.com>
2022-11-27 10:06:23 -0500

    Semana 5

--- Clase5/CTE.sql
+++ Clase5/CTE.sql
@@ -47,5 +47,84 @@ FROM RECURSOS.departamento D
 JOIN CTE1 ON D.IDDEPARTAMENTO = CTE1.IDDEPARTAMENTO;
 
 
+/*
+THONNY CRISTOFER FERNANDEZ CASA9:15
+yo utilice para hacer un datamart de fechas, de manera dinamica
+
+with rws as (  
+  select date'2021-01-01' + level - 1 as dt  
+  from   dual  
+  connect by level <= (  
+    date'2022-01-01' - date'2021-01-01'  
+  )    
+)  
+select * from rws;
+
+*/
+
+-- Ejercicio 2
+/*
+
+CIUDAD     EMPLEADOS      PORCENTAJE
+----------------------------------------------
+LIMA           50            22.72
+TRUJILLO       40            18.18
+AREQUIPA       70
+HUANCAYO       60
+----------------------------------------------
+              220           100.00
+*/
+
+/*
+THONNY CRISTOFER FERNANDEZ CASA9:46
+
+with emp_ciu as (
+select      ubc.idubicacion,
+            ubc.ciudad,
+            sum(1) as cantidad
+from        recursos.empleado emp
+            inner join recursos.departamento dpt on emp.iddepartamento = dpt.iddepartamento
+            inner join recursos.ubicacion ubc on dpt.idubicacion = ubc.idubicacion 
+group by    ubc.idubicacion, ubc.ciudad
+)
+select  ec.ciudad, 
+        ec.cantidad, 
+        round(ec.cantidad / sum(cantidad) over (partition by 1) * 100, 2) as porcentaje
+from    emp_ciu;
+*/
+
+WITH 
+CTE1 AS (
+	SELECT U.ciudad, COUNT(1) EMPLEADOS
+	FROM RECURSOS.empleado E
+	JOIN RECURSOS.departamento D ON E.iddepartamento = d.iddepartamento
+	JOIN RECURSOS.ubicacion U ON D.idubicacion = U.idubicacion
+	GROUP BY U.ciudad
+),
+CTE2 AS (
+	SELECT SUM(EMPLEADOS) TOTAL FROM CTE1
+)
+SELECT 
+	CTE1.CIUDAD, CTE1.EMPLEADOS, CTE2.TOTAL,
+	ROUND(CTE1.EMPLEADOS * 100.0 / CTE2.TOTAL,2)  PORCENTAJE	
+FROM CTE1 CROSS JOIN CTE2;
+
+
+/*
+THONNY CRISTOFER FERNANDEZ CASA9:59
+with emp_ciu as (
+select      distinct ubc.ciudad,
+            sum(1) over (partition by ubc.ciudad)  as cantidad,
+            sum(1) over (partition by 1) as total
+from        recursos.empleado emp
+            inner join recursos.departamento dpt on emp.iddepartamento = dpt.iddepartamento
+            inner join recursos.ubicacion ubc on dpt.idubicacion = ubc.idubicacion 
+)
+select  ciudad,
+        cantidad,
+        round(cantidad/total*100,2) as porcentaje
+from    emp_ciu ec;
+*/
+
 
 
